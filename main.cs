7064d4772adb5830c786a0205266359404cd071c
Yunhich <berezovskii05@mail.ru>
2024-10-29 19:37:48 +0700

    main.cs

--- main.cs
+++ main.cs
@@ -1,1 +1,35 @@
-a
+class Animal{
+  public string species;
+  public string foodinfo;
+  public string name;
+  public string habitat;
+  public int health;
+
+  public void action(string type){};
+{ 
+
+class Food{
+  public string typeOfFood;
+}
+
+class Decoration{
+  public string name;
+  public double square;
+}
+
+class Aviary{
+  public Food []foods;
+  public double square;
+}
+
+class Warehouse{
+  public Animal []animals;
+  public Decoration []decors;
+  public Food []foods;
+}
+
+class Zoo{
+  public Aviary []avlaries;
+  public Decoration []decors;
+  public Warehouse warehouse;
+}
